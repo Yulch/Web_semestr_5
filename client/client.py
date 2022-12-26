fd5731b9eeaf65aca1e85e6c965cd1ba9bf909f6
Yulch <82058787+Yulch@users.noreply.github.com>
2022-12-26 19:07:52 +0300

    Add files via upload

--- client/client.py
+++ client/client.py
@@ -1,8 +1,10 @@
 import requests
 #r = requests.get('http://127.0.0.1:5000/users')
 #print(r.json())
-r = requests.post('http://127.0.0.1:5000/users', json={'name': "Dilan", 'surname': "Nolan"})
+#r = requests.post('http://127.0.0.1:5000/users', json={'name': "Dilan", 'surname': "Nolan"})
 #print(r.json())
-#r = requests.delete('http://127.0.0.1:5000/users', json={'id': 5})
-#r = requests.put('http://127.0.0.1:5000/users', json={'id': 6, 'name': 'Chris', 'surname': 'Ouen' })
-#print(r.json())
+#r = requests.delete('http://127.0.0.1:5000/users', json={'id': 6})
+
+r = requests.put('http://127.0.0.1:5000/users', json={'id': 6, 'name': 'Chris', 'surname': 'Ouen' })
+print(r.json())
+#r = requests.get('http://127.0.0.1:5000/users')
--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import json
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, render_template, redirect
 import psycopg2
 import random
 app = Flask(__name__)
 data = []
+error_json = [{"code":113,"text":"Неверный идентификатор пользователя. ","solution":"Убедитесь, что Вы используете верный идентификатор."}]
 
 
 conn = psycopg2.connect(
@@ -14,6 +15,10 @@ conn = psycopg2.connect(
 )
 cursor = conn.cursor()
 
+
+@app.errorhandler(404)
+def user_not_found(e):
+    return render_template('404.html')
 
 
 @app.route('/users', methods=['GET'])
@@ -43,22 +48,30 @@ def add_user():
 
 @app.route('/users', methods=['DELETE'])
 def del_user():
-    sql = 'DELETE FROM public.users WHERE id=%s;'
-    id = request.get_json()['id']
-    cursor.execute(sql, (id,))
-    conn.commit()
-    return jsonify(data)
+    for i in data:
+        if request.get_json()['id'] == i['id']:
+            sql = 'DELETE FROM public.users WHERE id=%s;'
+            id = request.get_json()['id']
+            cursor.execute(sql, (id,))
+            conn.commit()
+            return jsonify(data)
+    return jsonify(error_json)
+
+
 
 
 @app.route('/users', methods=['PUT'])
 def update_user():
-    sql = "UPDATE public.users SET id = %s, name =%s, surname =%s WHERE id = %s;"
-    id = request.get_json()['id']
-    name = request.get_json()['name']
-    surname = request.get_json()['surname']
-    cursor.execute(sql, (id, name, surname, id))
-    conn.commit()
-    return jsonify(data)
+    for i in data:
+        if request.get_json()['id'] == i['id']:
+            sql = "UPDATE public.users SET id = %s, name =%s, surname =%s WHERE id = %s;"
+            id = request.get_json()['id']
+            name = request.get_json()['name']
+            surname = request.get_json()['surname']
+            cursor.execute(sql, (id, name, surname, id))
+            conn.commit()
+            return jsonify(data)
+    return jsonify(error_json)
 
 
 if __name__ == '__main__':
